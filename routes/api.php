c1070d35532ac8506408b0726ccf1f5bf1908322
ssahMzobe98 <75777459+ssahMzobe98@users.noreply.github.com>
2025-12-11 20:44:00 +0200

    removing un-used-api-for testing

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/learner-progress',[LearnersController::class,'getLearnersProgress']);
 Route::get('/active-courses/list',[CoursesController::class,'getActiveCourses']);
-Route::get('/learner-courses/{learner_id}',[LearnersController::class,'getLearnerCourses']);
-Route::get('/learner-progress/{learner_id}/{course_id}',[LearnersController::class,'getLearnerCourseProgress']);
 
